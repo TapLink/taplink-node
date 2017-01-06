0dc164d6189794987ba227aaeb9bc4719e6a7bfe
Jeremy Spilman <jeremy@taplink.co>
2017-01-06 11:11:08 -0500

    Add retry logic and stats tracking

--- taplink.js
+++ taplink.js
@@ -2,7 +2,7 @@
 // Version: NodeJS v1.0.1
 // Contact: support@taplink.co
 //
-// Copyright Â© 2016 TapLink, Inc
+// Copyright © 2016-2017 TapLink, Inc
 // MIT License
 
 var https  = require('https'),
@@ -19,6 +19,210 @@ var keepaliveAgent = new Agent({
 function TapLink(appId) {
     this.appId = appId;
     this.userAgent = "TapLink/1.0 nodejs/" + process.version.node;
+}
+
+function getSaltInternal(self, versionId, callback, saltRequest, hostList, requestNum) {
+    // Select the 'host' round-robin from the list of servers, based on the 'requestNum'
+    saltRequest.host = self.options.servers[requestNum % self.options.servers.length];
+
+    var startTime = Date.now();
+    var request = https.get(saltRequest, function(res) {
+        res.body = '';
+        res.on('data', function(chunk) { res.body += chunk; });
+        res.on('end', function() {
+            var latency = Date.now() - startTime;
+            startTime = null;
+
+            if (res.statusCode != 200) {
+                if (requestNum < self.options.retries) {
+                    statsError(self, saltRequest.host);
+                    return getSaltInternal(self, versionId, callback, saltRequest, hostList, ++requestNum);
+                } else {
+                    return callback(res);
+                }
+            }
+
+            try {
+                var response = JSON.parse(res.body);
+            } catch (err) {
+                if (requestNum < self.options.retries) {
+                    statsError(self, saltRequest.host);
+                    return getSaltInternal(self, versionId, callback, saltRequest, hostList, ++requestNum);
+                } else {
+                    return callback(res);
+                }
+            }
+
+            statsSuccess(self, saltRequest.host, latency);
+
+            // If 'versionId' specified in URL, then response may include upgrade info (new_vid, and new_s2)
+            // Otherwise, response will simply include 'vid' of the response
+            if (versionId == '')
+                return callback(null, response.s2, response.vid);
+            else
+                return callback(null, response.s2, versionId, response.new_s2, response.new_vid);
+        });
+    }).on('error', function(err) {
+        startTime = null;
+        if (requestNum < self.options.retries) {
+            statsError(self, saltRequest.host);
+            return getSaltInternal(self, versionId, callback, saltRequest, hostList, ++requestNum);
+        } else {
+            return callback(err);
+        }
+    });
+
+    request.setTimeout( self.options.timeout, function() {
+        // Some reports online that 'setTimeout' can fire even if 'end' has fired
+        // Check for 'startTime' to be defined to ensure that 'end' has not fired
+        if (startTime) {
+            if (requestNum < self.options.retries) {
+                statsTimeout(self, saltRequest.host);
+                getSaltInternal(self, versionId, callback, saltRequest, hostList, ++requestNum);
+            } else {
+                return callback('timeout');
+            }
+        }
+    });
+}
+
+function statsSuccess(self, host, latency) {
+    if (!self.options.stats) return;
+    if (!self.stats) statsInit(self);
+
+    var bucket = Math.round(latency / 20, 10);
+    bucket = bucket > 100 ? 101 : bucket;
+
+    self.stats.totalRequests[host]++;
+    self.stats.latency[host][bucket]++;
+
+    self.statsInt.oneMinuteRequests[host].push(Date.now());
+    self.statsInt.oneMinuteLatency[host].push([ Date.now(), latency ]);
+}
+
+function statsError(self, host) {
+    if (!self.options.stats) return;
+    if (!self.stats) statsInit(self);
+
+    self.stats.totalRequests[host]++;
+    self.stats.totalErrors[host]++;
+
+    self.statsInt.oneMinuteRequests[host].push(Date.now());
+    self.statsInt.oneMinuteErrors[host].push(Date.now());
+}
+
+function statsTimeout(self, host) {
+    if (!self.options.stats) return;
+    if (!self.stats) statsInit(self);
+
+    self.stats.totalRequests[host]++;
+    self.stats.totalTimeouts[host]++;
+
+    self.statsInt.oneMinuteRequests[host].push(Date.now());
+    self.statsInt.oneMinuteTimeouts[host].push(Date.now());
+}
+
+function statsInit(self) {
+    self.stats = {};
+    self.statsInt = {};
+
+    self.stats.latency = {};
+    self.stats.totalErrors = {};
+    self.stats.totalTimeouts = {};
+    self.stats.totalRequests = {};
+
+    self.statsInt.oneMinuteLatency = {};
+    self.statsInt.oneMinuteErrors = {};
+    self.statsInt.oneMinuteTimeouts = {};
+    self.statsInt.oneMinuteRequests = {};
+
+    self.stats.currentLatency = {};
+    self.stats.currentTimeouts = {};
+    self.stats.currentErrors = {};
+    self.stats.currentRequests = {};
+
+    self.options.servers.forEach(function (host) {
+        self.stats.latency[host] = new Array(102);
+        for (var i = 0; i < 102; ++i) self.stats.latency[host][i] = 0;
+
+        self.stats.totalErrors[host] = 0;
+        self.stats.totalTimeouts[host] = 0;
+        self.stats.totalRequests[host] = 0;
+
+        self.statsInt.oneMinuteRequests[host] = new Array(0);
+        self.statsInt.oneMinuteLatency[host] = new Array(0);
+        self.statsInt.oneMinuteErrors[host] = new Array(0);
+        self.statsInt.oneMinuteTimeouts[host] = new Array(0);
+    });
+}
+
+function agentStatus() {
+    console.log(keepaliveAgent.getCurrentStatus());
+    setTimeout(agentStatus, 10000);
+}
+
+function statsTracking(self) {
+    if (self.options.stats) {
+        // Update one minute running totals and averages
+        var now = Date.now();
+
+        self.options.servers.forEach(function (host) {
+            // LATENCY
+            // The latency list for each host is an array of [ Date.now, latency ]
+            // For each host, we need to remove entries older than one minute, and then avaerage the rest
+            var hostStats = self.statsInt.oneMinuteLatency[host];
+
+            // Remove entries older than one minute (from the front of the array)
+            while (hostStats.length > 0 && (now - hostStats[0][0]) > 60000) {
+                hostStats.shift();
+            }
+
+            // Average the remaining entries
+            if (hostStats.length > 0) {
+                // Reduce takes each element in array and accumulates it into 'a'
+                // Note each 'b' is [ Date.now, latency ] so we want b[1]
+                // and we set 'a' to start at 0
+                var sum = hostStats.reduce(function(a, b) { return a + b[1]; }, 0);
+                var avg = sum / hostStats.length;
+                self.stats.currentLatency[host] = avg;
+            } else {
+                self.stats.currentLatency[host] = null;
+            }
+
+            // REQUESTS
+            // The request list for each host is a list of timestamps of when the request occurred
+            hostStats = self.statsInt.oneMinuteRequests[host];
+            while (hostStats.length > 0 && (now - hostStats[0]) > 60000) {
+                hostStats.shift();
+            }
+            self.stats.currentRequests[host] = hostStats.length;
+
+            // ERRORS
+            // The error list for each host is a list of timestamps of when the error occurred
+            hostStats = self.statsInt.oneMinuteErrors[host];
+            while (hostStats.length > 0 && (now - hostStats[0]) > 60000) {
+                hostStats.shift();
+            }
+            self.stats.currentErrors[host] = hostStats.length;
+
+            // TIMEOUTS
+            // The timeout list for each host is a list of timestamps of when the timeout occurred
+            hostStats = self.statsInt.oneMinuteTimeouts[host];
+            while (hostStats.length > 0 && (now - hostStats[0]) > 60000) {
+                hostStats.shift();
+            }
+            self.stats.currentTimeouts[host] = hostStats.length;
+        });
+
+        // Last Step: Re-order the server list in order of reliability & performance
+        //   1) Calculate error rate and order by error rate
+        //   2) For equal error rates (e.g. zero errors) order by latency
+    } else {
+        self.stats = null;
+        self.statsInt = null;
+    }
+
+    setTimeout(function() { statsTracking(self) }, 10000);
 }
 
 TapLink.prototype = {
@@ -40,7 +244,12 @@ TapLink.prototype = {
             res.on('end', function() {
                 if (res.statusCode != 200) return callback(res);
                 try {
-                    self.options = JSON.parse(res.body)
+                    self.options = JSON.parse(res.body);
+
+                    // Default options, if not provided by server
+                    self.options.timeout = self.options.timeout || 500;
+                    self.options.retries = self.options.retries || 3;
+                    self.options.stats   = self.options.stats   || 0
                 } catch (err) {
                     return callback(err);
                 }
@@ -51,6 +260,8 @@ TapLink.prototype = {
         }).on('error', function(err) {
             return callback(err);
         });
+
+        setTimeout(function() { statsTracking(self) }, 1000);
     },
 
     // Verify a password for an existing user which was stored using blind hashing.
@@ -109,7 +320,8 @@ TapLink.prototype = {
     // Inputs:
     //    'hash1Hex'  - hex string containing value of hash1
     //    'versionId' - version identifier for data pool settings to use, or 0/null/undefined to use latest settings
-    //    'callback'  - function(salt2Hex, versionId, newSalt2Hex, newVersionId)
+    //    'callback'  - function(err, salt2Hex, versionId, newSalt2Hex, newVersionId)
+    //       o err          : error value, or null if request succeeded
     //       o salt2Hex     : hex string containing value of 'salt2'
     //       o versionId    : version id corresponding to the provided 'salt2Hex' value (will always match requested version, if one was specified)
     //       o newSalt2Hex  : hex string containing a new value of 'salt2' if newer data pool settings are available, otherwise undefined
@@ -119,7 +331,6 @@ TapLink.prototype = {
             versionId = ''
 
         var saltRequest = {
-            host: 'api.taplink.co',
             path: '/' + this.appId + '/' + hash1Hex + '/' + versionId,
             headers: {
                 'User-Agent': this.userAgent,
@@ -128,34 +339,9 @@ TapLink.prototype = {
             agent: keepaliveAgent
         }
 
-        https.get(saltRequest, function(res) {
-            res.body = '';
-            res.on('data', function(chunk) { res.body += chunk; });
-            res.on('end', function() {
-                if (res.statusCode != 200) return callback(res);
-
-                try {
-                    var response = JSON.parse(res.body);
-                } catch (err) {
-                    return callback(res);
-                }
-
-                // If 'versionId' specified in URL, then response may include upgrade info (new_vid, and new_s2)
-                // Otherwise, response will simply include 'vid' of the response
-                if (versionId == '')
-                    callback(null, response.s2, response.vid);
-                else
-                    callback(null, response.s2, versionId, response.new_s2, response.new_vid);
-            });
-        }).on('error', function(err) {
-            return callback(err);
-        });
+        var hostList = this.options.servers.slice(0, this.options.servers.length);
+        getSaltInternal(this, versionId, callback, saltRequest, hostList, 0);
     }
-}
-
-function agentStatus() {
-    console.log(keepaliveAgent.getCurrentStatus());
-    setTimeout(agentStatus, 10000);
 }
 
 module.exports = TapLink;
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 // TapLink Blind Hashing - NodeJS Stress Test
 //
 // Queues 128 ongoing blind hashing requests which will be processed by
-// the client library by spreading requests across multiple persistent 
+// the client library by spreading requests across multiple persistent
 // sockets with keepalive.
 //
 // Version: v0.0.1
 // Contact: support@taplink.co
 //
-// Copyright Â© 2016 TapLink, Inc
+// Copyright © 2016 TapLink, Inc
 // MIT License
 
 var TapLink = require('./taplink.js'),
@@ -17,51 +17,55 @@ var tapLinkAppID = process.env['TAPLINK_APPID'];
 var tapLinkClient = new TapLink(tapLinkAppID);
 
 function getTimestamp() {
-	var ts = new Date();
-	return ("0" + ts.getHours()).slice(-2) + ':' +
-		   ("0" + ts.getMinutes()).slice(-2) + ':' +
-    	   ("0" + ts.getSeconds()).slice(-2) + '.' +
-    	   ("00" + ts.getMilliseconds()).slice(-3);
+        var ts = new Date();
+        return ("0" + ts.getHours()).slice(-2) + ':' +
+                   ("0" + ts.getMinutes()).slice(-2) + ':' +
+           ("0" + ts.getSeconds()).slice(-2) + '.' +
+           ("00" + ts.getMilliseconds()).slice(-3);
 }
 
 function tapLinkInit(err) {
-	if (err) {
-		console.log(err);
-		setTimeout(tapLinkClient.init(tapLinkInit), 1000);
-		return;
-	}
+        if (err) {
+                console.log(err);
+                setTimeout(tapLinkClient.init(tapLinkInit), 1000);
+                return;
+        }
 
-	for (var i = 0; i < 128; i++) {
-		setTimeout(beginTest(), 10);
-	}
+        tapLinkClient.options.stats = 1;
+
+        for (var i = 0; i < 128; i++) {
+                setTimeout(beginTest(), 10);
+        }
 }
 
 var counter = 0;
 var count = {};
 
 function beginTest() {
-	var hash = crypto.createHash('sha512').update(counter.toString()).digest('hex');
-	tapLinkClient.getSalt(hash, null, function(err, salt2Hex, versionId) {
-		if (counter % 10000 == 0) {
-			console.log(getTimestamp(), counter);
-			Object.keys(count).forEach(function(key) {
-				console.log("            ", count[key], ":", key)
-			});
-		}
-		if (err) {
-			if (err.statusCode && err.body) {
-				var key = err.statusCode + '-' + err.body.trim();
-				if (!count[key]) count[key] = 1; else count[key]++;
-			} else if (err.code = 'ECONNRESET') {
-				if (!count[err.code]) count[err.code] = 1; else count[err.code]++;
-			} else {
-				console.log(getTimestamp(), counter, err);
-			}
-		}
+        var hash = crypto.createHash('sha512').update(counter.toString()).digest('hex');
+        tapLinkClient.getSalt(hash, null, function(err, salt2Hex, versionId) {
+                if (counter % 10000 == 0) {
+                        console.log(getTimestamp(), counter);
+                        Object.keys(count).forEach(function(key) {
+                                console.log("            ", count[key], ":", key)
+                        });
 
-		counter++;
-		beginTest();
-	});
+                        console.log(JSON.stringify(tapLinkClient.stats));
+                }
+                if (err) {
+                        if (err.statusCode && err.body) {
+                                var key = err.statusCode + '-' + err.body.trim();
+                                if (!count[key]) count[key] = 1; else count[key]++;
+                        } else if (err.code = 'ECONNRESET') {
+                                if (!count[err.code]) count[err.code] = 1; else count[err.code]++;
+                        } else {
+                                console.log(getTimestamp(), counter, err);
+                        }
+                }
+
+                counter++;
+                beginTest();
+        });
 }
 
 tapLinkClient.init(tapLinkInit)
